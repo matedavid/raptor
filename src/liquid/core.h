9382e64cd9f111839dc0394329208bdbdfd6e99a
matedavid <david.mate.lopez@gmail.com>
2022-10-23 01:37:58 +0200

    added spdlog for logging in liquid

--- src/liquid/core.h
+++ src/liquid/core.h
@@ -1,17 +1,22 @@
 #pragma once
 
 #include <memory>
+#include "spdlog/spdlog.h"
 
 namespace liquid {
 
-template <typename T>
+#define LOG_INFO(...)  spdlog::info(__VA_ARGS__)
+#define LOG_WARN(...)  spdlog::warn(__VA_ARGS__)
+#define LOG_ERROR(...) spdlog::error(__VA_ARGS__)
+
+template<typename T>
 using Ref = std::shared_ptr<T>;
 
 #define MakeRef(T, ...) std::make_shared<T>(__VA_ARGS__)
 
-template <typename T>
+template<typename T>
 using Scope = std::unique_ptr<T>;
 
 #define MakeScope(T, ...) std::make_unique<T>(__VA_ARGS__)
 
-}
+} // namespace liquid
