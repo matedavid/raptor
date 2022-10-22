24398c0137616462cd37647801cd7359b508be67
matedavid <david.mate.lopez@gmail.com>
2022-10-22 19:24:22 +0200

    fixed typo and created helper macros for creating Refs and Scopes

--- src/liquid/core.h
+++ src/liquid/core.h
@@ -7,7 +7,11 @@ namespace liquid {
 template <typename T>
 using Ref = std::shared_ptr<T>;
 
+#define MakeRef(T, ...) std::make_shared<T>(__VA_ARGS__)
+
 template <typename T>
-using Scpe = std::unique_ptr<T>;
+using Scope = std::unique_ptr<T>;
+
+#define MakeScope(T, ...) std::make_unique<T>(__VA_ARGS__)
 
 }
